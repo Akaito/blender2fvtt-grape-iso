182558642817c60c432a6cebec98ae93e8a2832f
Chris Barrett <chris@codesaru.com>
2020-12-14 00:33:55 -0800

    Change format to group f walls by b walls.
    Group foundry wall output by blender 'wall*' object input.

--- export.py
+++ export.py
@@ -1,3 +1,7 @@
+# Terminology:
+#   blender wall: an object named `wall*` in Blender.
+#   foundry wall: a start/end point pair to be made in Foundry.
+
 import bpy
 import json
 import mathutils
@@ -19,45 +23,6 @@ print('~*~ EXPORT.PY ~*~')
 
 print(dir(context))
 print(dir(scene))
-
-'''
-def get_wall_endpoints(obj):
-    print('getting endpoints for {}'.format(obj.name))
-    print(obj.dimensions)
-    world_mtx = obj.matrix_world
-    print(world_mtx)
-    print(world_mtx.translation)
-    obj_origin__world = obj.matrix_world.translation
-    obj_dims = obj.dimensions
-    return [
-            {obj_origin__world.x, obj_origin__world.y, obj_origin__world.z},
-            ]
-'''
-
-def export_wall(jsn, obj):
-    print('export a wall')
-    #print(obj.matrix_basis)
-    #print(obj.matrix_local)
-    #print(obj.matrix_world)
-    dims_vec = mathutils.Vector((obj.dimensions.x, obj.dimensions.y, obj.dimensions.z))
-    origin = obj.matrix_world.translation
-    # Transform dimensions (the object-local bounding box)
-    #   by the object's rotation.
-    dimensions__world = (obj.matrix_basis.decompose()[1]) @ dims_vec
-    jsn.append({
-        'name': obj.name, # for hooking up tiles/textures later
-        #'texture': obj.data.image?
-        'origin': [
-            round(origin.x, 6),
-            round(origin.y, 6),
-            round(origin.z, 6),
-            ],
-        'farPoint': [
-            round(origin.x + dimensions__world.x, 6),
-            round(origin.y + dimensions__world.y, 6),
-            round(origin.z + dimensions__world.z, 6),
-            ],
-        })
 
 
 def vec_center(vec_list):
@@ -84,7 +49,7 @@ def main():
     camera = scene.collection.all_objects.get(CAMERA_NAME)
     assert camera is not None, 'Please have a camera named {}'.format(CAMERA_NAME)
     jsn = {
-            'walls': []
+            'blenderWalls': []
     }
 
     objects = scene.collection.all_objects
@@ -99,6 +64,8 @@ def main():
             bbox_center__world, bbox_center_bottom__world = vec_center_bottom(object.bound_box, object.matrix_world)
             print('bbox_center__world: {}'.format(bbox_center__world))
             print('     bottom:        {}'.format(bbox_center_bottom__world))
+
+            walls = []
             for edge in mesh.edges:
                 edge_v0__local = mathutils.Vector(mesh.vertices[edge.vertices[0]].co)
                 edge_v1__local = mathutils.Vector(mesh.vertices[edge.vertices[1]].co)
@@ -135,8 +102,7 @@ def main():
                 print('midpoint world: {}'.format(edge_midpoint__world))
                 print('is front? {}'.format(is_front_facing))
 
-                jsn['walls'].append({
-                    'blenderObjectName': object.name, # for hooking up tiles/textures later
+                walls.append({
                     #'texture': obj.data.image?
                     'isFrontFacing': is_front_facing,
                     'a': [
@@ -150,7 +116,11 @@ def main():
                         round(edge_v1__world.z, 6),
                         ],
                 })
-            #return
+            if len(walls) > 0:
+                jsn['blenderWalls'].append({
+                    'blenderObjectName': object.name,
+                    'foundryWalls': walls,
+                })
 
     print(json.dumps(jsn, indent=2, sort_keys=True))
 main()
